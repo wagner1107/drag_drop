89ed407e000bd0ea942e799cf9169292ee57cc6e
Wagner Augusto <wagner1107@hotmail.com>
2018-09-27 18:46:02 -0300

    Arrastar elemento

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style type="text/css">
+		#objeto{
+			width: 150px;
+			height: 150px;
+			background-color: #000;
+			position: absolute;
+			cursor: move;
+		}
+	</style>
+</head>
+<body>
+	<div id="objeto"></div>
+
+
+	<script type="text/javascript" src="jquery.min.js"></script>
+	<script type="text/javascript" src="script.js"></script>
+</body>
+</html>
